a78ebf73e9f1a5bdb369b134c2e5da327a2254be
mkol5222 <mkoldov@gmail.com>
2023-09-25 06:57:59 +0000

    .

--- post-config.sh
+++ post-config.sh
@@ -23,18 +23,35 @@ setup_run_on_reboot() {
     fi
 }
 
-run_only_once() {
-    local postconfigflagfile=/var/run/post_config_completed
+call_once() {
+    echo Once
+    date
+    touch /var/log/hello.txt
+    #cd /var/opt/CPsuite-R81.10/fw1/conf/; cp azure-ha.json azure-ha.json.orig; cat azure-ha.json.orig | jq -r '.clusterNetworkInterfaces.nic1=.clusterNetworkInterfaces.eth0 | del(.clusterNetworkInterfaces.eth0)' > azure-ha.json
+    #cd /var/opt/CPsuite-R81.10/fw1/conf/; cp azure-ha.json azure-ha.json.orig; cat azure-ha.json.orig | jq -r '.clusterNetworkInterfaces.nic1=.clusterNetworkInterfaces.eth0 | del(.clusterNetworkInterfaces.eth0) | del(.clusterNetworkInterfaces.nic1[].pub)' > azure-ha.json
+    echo Once done
+}
+
+call_always() {
+    echo Always
+    date
+    echo Always done
+}
+
+run() {
+    local postconfigflagfile=/var/log/post_config_completed
     if [ ! -f  $postconfigflagfile ]; then 
         # Code to run only on the first boot 
         # ... 
         echo "Running only once $(date)" >> /var/log/run_once.log
+        call_once 2>&1 1>/var/log/run_once.log
         
         # Create the flag file to indicate that the code has been executed 
         touch $postconfigflagfile
     fi
 
     echo "Running on each boot $(date)" >> /var/log/run_on_boot.log
+    call_always 2>&1 1>/var/log/run_on_boot.log
 }
 
 
@@ -44,7 +61,7 @@ main() {
 
     if [ -f /etc/rc3.d/S99$base ]; then
         print_info "Run on reboot already setup"
-        run_only_once
+        run
     else
         print_info "Setting up run on reboot"
         setup_run_on_reboot
